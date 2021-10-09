9f3793651b216f1b5949e16c0fdb50a82fe0ace6
Jay Ranpariya <jayranapriya04@gmail.com>
2021-10-10 01:20:14 +0530

    stripe

--- app.js
+++ app.js
@@ -1,50 +1,54 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
-const express = require('express');
+require("dotenv").config();
+const mongoose = require("mongoose");
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 //my router
-const authRoutes = require('./routes/auth')
-const userRoutes = require('./routes/user')
-const categoryRoutes = require('./routes/category')
-const productRoutes = require('./routes/product')
-const orderRoutes = require('./routes/order')
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
+const orderRoutes = require("./routes/order");
+const stripeRoutes = require("./routes/stripepayment");
 
 //db connection
-mongoose.connect(process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }).then(() => {
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
     console.log("DB connected");
-}).catch(
-    () => {
-        console.log("opsssss")
-    }
-);
+  })
+  .catch(() => {
+    console.log("opsssss");
+  });
 
 // middleware
-app.use(bodyParser.json())
-app.use(cookieParser())
-app.use(cors())
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
 
-
-app.get('/', (req, res) => {
-    res.send("jay")
-})
+app.get("/", (req, res) => {
+  res.send("jay");
+});
 
 //my router
-app.use("/api", authRoutes)
-app.use("/api", userRoutes)
-app.use("/api", categoryRoutes)
-app.use("/api", productRoutes)
-app.use("/api", orderRoutes)
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
+app.use("/api", orderRoutes);
+app.use("/api", stripeRoutes);
 
 //port
 const port = process.env.PORT || 8000;
 
-
 //starting server
 app.listen(port, () => {
-    console.log(`app is running at ${port}`);
-})
+  console.log(`app is running at ${port}`);
+});
